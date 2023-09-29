9eaf2bae0be75f20beaaa998928ba2de94ce91b4
Vaaruna2709 <143427526+Vaaruna2709@users.noreply.github.com>
2023-09-29 11:01:53 +0530

    Update to print no of days in a month.cpp

--- to print no of days in a month.cpp
+++ to print no of days in a month.cpp
@@ -1,55 +1,25 @@
-#include <stdio.h>
-#include <math.h>
+#include<stdio.h>
+#include<math.h>
 
-int main() {
-	int y,m;
+int main(){
+	int m,y;
 	printf("enter the year and month:");
 	scanf("%d""%d",&y,&m);
-	switch(m){
-		case 1:
-			printf("31 days in this month");
-			break;
-		case 2:
-			if (y%4==0 && (y%400==0 || y%100 !=0)){
-			
-			printf("29 days in this month");
-		}
-			else{
-			
-			printf("28 days in this month");
-		}
-			break;
-		case3:
-			printf("31 days in this month");
-			break;
-		case 4:
-			printf("30 days in this month");
-			break;
-		case 5:
-			printf("31 days in this month");
-			break;
-		case 6:
-			printf("30 days in this month");
-			break;
-		case 7:
-			printf("31 days in this month");
-			break;
-		case 8:
-			printf("31 days in this month");
-			break;
-		case 9:
-			printf("30 days in this month");
-			break;
-		case 10:
-			printf("31 days in this month");
-			break;
-		case 11:
-			printf("30 days in this month");
-			break;
-		case 12:
-			printf("31 days in this month");
-			break;
-			
+	
+	if(m==1 || m ==3 || m== 5 || m == 7 || m== 8 || m == 10 || m==12)
+	printf("31days in this month");
+	
+	else if(m==2){
+		if (y%4==0 && (y%400==0 || y%100 !=0))
+		printf("29 days in this month");
+		
+		else
+		printf("28 days in this month");
 	}
+	else if(m==4 || m ==6 || m== 9 || m == 11 )
+	printf("30 days in this month");
+	else
+	printf("enter a valid number");
+	
 	return 0;
 }
