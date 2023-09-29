3841be16b24154348f1d523db2dcd0b8c2376883
Vaaruna2709 <143427526+Vaaruna2709@users.noreply.github.com>
2023-09-29 11:05:17 +0530

    Update to print no of days in a month.cpp

--- to print no of days in a month.cpp
+++ to print no of days in a month.cpp
@@ -7,7 +7,7 @@ int main(){
 	scanf("%d""%d",&y,&m);
 	
 	if(m==1 || m ==3 || m== 5 || m == 7 || m== 8 || m == 10 || m==12)
-	printf("31days in this month");
+	printf("31 days in this month");
 	
 	else if(m==2){
 		if (y%4==0 && (y%400==0 || y%100 !=0))
