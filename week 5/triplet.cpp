8a3ee5e6347defa858e05ca1706106e631493ca3
Vaaruna2709 <143427526+Vaaruna2709@users.noreply.github.com>
2023-11-05 22:07:32 +0530

    Update triplet.cpp

--- week 5/triplet.cpp
+++ week 5/triplet.cpp
@@ -11,13 +11,13 @@ int main(){
 	
 	printf("enter the element to be checked:");
 	scanf("%d",&k);
-	for(int i=0;i<n;i++){
+	for(int i=0;i<n-2;i++){
 		for(int j=i+1;j<n-1;j++){
-			for(int m=i+2;m<n-2;m++){
-				if(k==a[i]+a[j]+a[m])
+			for(int m=j+1;m<n;m++){
+				if(k==a[i]+a[j]+a[m]){
 				printf("the triplet is %d %d %d \n",a[i],a[j],a[m]);
 				t++;
-			}
+			}}
 		}
 	}
 	if(t==0)
